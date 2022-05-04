0ac6ea9300dbe5a7ebb409bd110fc8cd6c8cbc70
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-04 17:21:28 +0000

    Create handleSubmit, fetchUser and fetchRepos functions

--- src/context.js
+++ src/context.js
@@ -1,27 +1,52 @@
 import React, {useState, useContext} from "react";
+import {Octokit} from '@octokit/core';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [user, setUser] = useState(null)
-  const [repos, setRepos] = useState(null)
-  const [searchQuery, setSearchQuery] = useState('')
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const fetchData = async (url) => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data)
-    } catch (e) {
-      console.error(e)
-    }
+  // Setting up Octokit default parameters
+  const octokit = new Octokit({
+    auth: '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',
+    accept: 'application/vnd.github.v3+json'
+  });
+
+  // Function for fetching users
+  const fetchUser = async () => {
+    setIsLoading(true);
+
+    const {data} = await octokit.request('GET /users/{username}', {
+      username: searchQuery,
+    });
+    const {followers, following, login, avatar_url:avatarUrl, name, html_url:htmlUrl} = data;
+
+    setIsLoading(false);
+    return {followers, following, login, avatarUrl, name, htmlUrl}
   }
 
-  const handleSubmit = (e) => {
+  // Function for fetching user repos
+  const fetchRepos = async () => {
+    setIsLoading(true);
+
+    const {data} = await octokit.request('GET /users/{username}/repos', {
+      username: searchQuery,
+    });
+
+    setIsLoading(false);
+    return data;
+  }
+
+  // Function for control form submit
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(e.target.value)
-    fetchData()
+    const newUser = await fetchUser();
+    const newRepos = await fetchRepos();
+    setUser(newUser);
+    setRepos(newRepos);
   }
 
   return (
@@ -31,6 +56,7 @@ const AppProvider = ({children}) => {
   )
 }
 
+// Setting up our custom hook for Context API
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
