f9e02042a4a9eca3199f6a762f423328797c40d7
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-05 17:49:31 +0000

    Create spinner component and styles

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,14 @@
 import {useGlobalContext} from "../../context";
+import Spinner from "../../components/spinner/spinner.component";
 import InitialState from "../../components/initialState/initialState.component";
 import ErrorState from "../../components/errorState/errorState.component";
 import User from "../../components/user/user.component";
 import Repos from "../../components/repos/repos.component";
 
 const HomePage = () => {
-  const {searchQuery, isError, user} = useGlobalContext()
+  const {isLoading, searchQuery, isError, user} = useGlobalContext()
+
+  if (isLoading) return <Spinner />
 
   if (!searchQuery && !user) return <InitialState/>
 
