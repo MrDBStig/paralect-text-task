c67a84ecb5ba664a1b2af5c1bc936aca2d4d9015
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-18 18:48:20 +0000

    Refactor empty state condition show when user is already set

--- src/components/repos/repos.component.jsx
+++ src/components/repos/repos.component.jsx
@@ -1,31 +1,37 @@
-import {useGlobalContext} from "../../context";
+import { useGlobalContext } from "../../context";
 import EmptyList from "../emptyList/emptyList.component";
 import Repository from "../repository/repository.component";
 import Pagination from "../pagination/pagination.component";
 
 const Repos = () => {
-  const {user, repos, REPOS_PER_PAGE} = useGlobalContext();
+  const { user, repos, REPOS_PER_PAGE } = useGlobalContext();
 
   // When user doesn't have public repos or repos at all
-  if (user && !repos) {
-    return <section className="empty">
-      <EmptyList/>
-    </section>
+  if (repos.length < 1 && user) {
+    return (
+      <section className="empty">
+        <EmptyList />
+      </section>
+    );
   }
 
   // When user have at least 1 public repo
-  if (repos) {
-    const {publicRepos} = user;
+  if (repos.length >= 1) {
+    const { publicRepos } = user;
 
-    return <section className="repos">
-      <h2 className="repos__title">Repositories ({publicRepos})</h2>
-      <div className="repos__list">
-        {repos.map((rep) => <Repository key={rep.id} {...rep} />)}
-      </div>
-      {/* Display pagination only when user has more than 4 repos */}
-      {publicRepos > REPOS_PER_PAGE && <Pagination/>}
-    </section>;
+    return (
+      <section className="repos">
+        <h2 className="repos__title">Repositories ({publicRepos})</h2>
+        <div className="repos__list">
+          {repos.map((rep) => (
+            <Repository key={rep.id} {...rep} />
+          ))}
+        </div>
+        {/* Display pagination only when user has more than 4 repos */}
+        {publicRepos > REPOS_PER_PAGE && <Pagination />}
+      </section>
+    );
   }
-}
+};
 
 export default Repos;
