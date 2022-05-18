6bc87e1bbcc592efe16bd220803b9f00c40835b7
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-18 19:17:52 +0000

    Add autofocus to search input when app is loaded

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,26 @@
-import {useGlobalContext} from "../../context";
-import {GithubIcon} from "../../img/github-icon";
+import { useGlobalContext } from "../../context";
+import { GithubIcon } from "../../img/github-icon";
 
 const Header = () => {
-  const {searchQuery, setSearchQuery, handleSubmit} = useGlobalContext()
+  const { searchQuery, setSearchQuery, handleSubmit } = useGlobalContext();
 
   return (
     <header className="header">
       <div className="container">
-        <GithubIcon/>
+        <GithubIcon />
         <form className="search-form" onSubmit={handleSubmit}>
-          <input type="text" className="search-form__input" value={searchQuery} placeholder="Enter GitHub Username"
-                 onChange={(e) => setSearchQuery(e.target.value)}/>
+          <input
+            type="text"
+            autoFocus={true}
+            className="search-form__input"
+            value={searchQuery}
+            placeholder="Enter GitHub Username"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </form>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
